d96efb5ceba31255ed9c9e6b5500ac599b87a316
Lee Allen <leeallen337@gmail.com>
2016-12-20 16:41:50 -0500

    Finishing up variables section

--- index.js
+++ index.js
@@ -136,17 +136,17 @@ module.exports = {
     "init-declarations": "off",                                                   // http://eslint.org/docs/rules/init-declarations
     "no-catch-shadow": "off",                                                     // http://eslint.org/docs/rules/no-catch-shadow
     "no-delete-var": "error",                                                     // http://eslint.org/docs/rules/no-delete-var
-    "no-label-var": "warn",                                                       // http://eslint.org/docs/rules/no-label-var
+    "no-label-var": "off",                                                        // http://eslint.org/docs/rules/no-label-var
     "no-restricted-globals": "off",                                               // http://eslint.org/docs/rules/no-restricted-globals
     "no-shadow-restricted-names": "error",                                        // http://eslint.org/docs/rules/no-shadow-restricted-names
     "no-shadow": "error",                                                         // http://eslint.org/docs/rules/no-shadow
     "no-undef-init": "error",                                                     // http://eslint.org/docs/rules/no-undef-init
     "no-undef": "error",                                                          // http://eslint.org/docs/rules/no-undef
     "no-undefined": "error",                                                      // http://eslint.org/docs/rules/no-undefined
-    "no-unused-vars": ["error", {
+    "no-unused-vars": ["error", {                                                 // http://eslint.org/docs/rules/no-unused-vars
       "vars": "local",
       "args": "after-used"
-    }],                                                                           // http://eslint.org/docs/rules/no-unused-vars
+    }],
     "no-use-before-define": "error",                                              // http://eslint.org/docs/rules/no-use-before-define
 
     /**
