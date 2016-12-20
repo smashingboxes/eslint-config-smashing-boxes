1032fae756f48dc6cb14788f070054d0764e4461
Lee Allen <leeallen337@gmail.com>
2016-12-20 16:00:04 -0500

    Adding in all possible errors section

--- index.js
+++ index.js
@@ -19,11 +19,35 @@ module.exports = {
      */
     "no-await-in-loop": "off",                                                    // http://eslint.org/docs/rules/no-await-in-loop
     "no-cond-assign": ["error", "always"],                                        // http://eslint.org/docs/rules/no-cond-assign
-    "no-console": "warn",                                                         // http://eslint.org/docs/rules/no-console
-    "no-constant-condition": "warn",                                              // http://eslint.org/docs/rules/no-constant-condition
-    "no-control-regex": "off",                                                    // http://eslint.org/docs/rules/no-control-regex
-    "no-debugger": "warn",                                                        // http://eslint.org/docs/rules/no-debugger
-
+    "no-console": "warning",                                                      // http://eslint.org/docs/rules/no-console
+    "no-constant-condition": "warning",                                           // http://eslint.org/docs/rules/no-constant-condition
+    "no-control-regex": "error",                                                  // http://eslint.org/docs/rules/no-control-regex
+    "no-debugger": "warning",                                                     // http://eslint.org/docs/rules/no-debugger
+    "no-dupe-args": "error",                                                      // http://eslint.org/docs/rules/no-dupe-args
+    "no-dupe-keys": "error",                                                      // http://eslint.org/docs/rules/no-dupe-keys
+    "no-duplicate-case": "error",                                                 // http://eslint.org/docs/rules/no-duplicate-case
+    "no-empty-character-class": "error",                                          // http://eslint.org/docs/rules/no-empty-character-class
+    "no-empty": "error",                                                          // http://eslint.org/docs/rules/no-empty
+    "no-ex-assign": "error",                                                      // http://eslint.org/docs/rules/no-ex-assign
+    "no-extra-boolean-cast": "off",                                               // http://eslint.org/docs/rules/no-extra-boolean-cast
+    "no-extra-parens": ["error", "functions"],                                    // http://eslint.org/docs/rules/no-extra-parens
+    "no-extra-semi": "error",                                                     // http://eslint.org/docs/rules/no-extra-semi
+    "no-func-assign": "error",                                                    // http://eslint.org/docs/rules/no-func-assign
+    "no-inner-declarations": "error",                                             // http://eslint.org/docs/rules/no-inner-declarations
+    "no-invalid-regexp": "error",                                                 // http://eslint.org/docs/rules/no-invalid-regexp
+    "no-irregular-whitespace": "error",                                           // http://eslint.org/docs/rules/no-irregular-whitespace
+    "no-obj-calls": "error",                                                      // http://eslint.org/docs/rules/no-obj-calls
+    "no-prototype-builtins": "error",                                             // http://eslint.org/docs/rules/no-prototype-builtins
+    "no-regex-spaces": "error",                                                   // http://eslint.org/docs/rules/no-regex-spaces
+    "no-sparse-arrays": "error",                                                  // http://eslint.org/docs/rules/no-sparse-arrays
+    "no-template-curly-in-string": "warning",                                     // http://eslint.org/docs/rules/no-template-curly-in-string
+    "no-unexpected-multiline": "error",                                           // http://eslint.org/docs/rules/no-unexpected-multiline
+    "no-unreachable": "error",                                                    // http://eslint.org/docs/rules/no-unreachable
+    "no-unsafe-finally": "error",                                                 // http://eslint.org/docs/rules/no-unsafe-finally
+    "no-unsafe-negation": "error",                                                // http://eslint.org/docs/rules/no-unsafe-negation
+    "use-isnan": "error",                                                         // http://eslint.org/docs/rules/use-isnan
+    "valid-jsdoc": "error",                                                       // http://eslint.org/docs/rules/valid-jsdoc
+    "valid-typeof": "error",                                                      // http://eslint.org/docs/rules/valid-typeof
 
     /**
      * Best Practices
